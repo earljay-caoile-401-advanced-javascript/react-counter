cbc69132d67b440b02fbe7ea3cad53a376359f12
Earl Jay Caoile <unfie555@gmail.com>
2020-06-08 22:35:44 -0700

    imported code from sandbox

---
+++ src/components/Header.js
@@ -0,0 +1,20 @@
+import React from "react";
+
+/**
+ * Component for the header content
+ * 
+ * @component
+ * @example
+ * return (
+  *   <Header />
+  * )
+  */
+function Header() {
+  return (
+    <div className="header">
+      <h1>Lab 21 — Component Based UI</h1>
+    </div>
+  );
+}
+
+export default Header;
