908c496513712924f9b262f3b698890acd490a81
Earl Jay Caoile <unfie555@gmail.com>
2020-06-11 22:18:21 -0700

    added counter appearance tests

--- src/__tests__/Counter.test.js
+++ src/__tests__/Counter.test.js
@@ -1,8 +1,10 @@
 import React from 'react';
 import { shallow, mount, render } from 'enzyme';
 import Counter from '../components/Counter';
+import Header from '../components/Header';
+import Footer from '../components/Footer';
 
-describe('does component work right', () => {
+describe('counter component', () => {
   it('has right initial state values', () => {
     const component = shallow(<Counter />);
     expect(component.state('count')).toBe(0);
@@ -11,7 +13,7 @@ describe('does component work right', () => {
   it('updates the state correctly when we click the + button', () => {
     const component = shallow(<Counter />);
     const plusBtn = component.find('span.up');
-    
+
     plusBtn.simulate('click');
     expect(component.state('count')).toBe(1);
 
@@ -28,5 +30,38 @@ describe('does component work right', () => {
 
     minusBtn.simulate('click');
     expect(component.state('count')).toBe(-2);
+  });
+});
+
+describe('counter component appearance', () => {
+  it('looks right', () => {
+    const component = render(<Counter />);
+
+    const minus = component.find('span.down');
+    expect(minus.text()).toBe(' - ');
+
+    const count = component.find('span.count');
+    expect(count.text()).toBe('0');
+
+    const plus = component.find('span.up');
+    expect(plus.text()).toBe(' + ');
+  });
+});
+
+describe('header component', () => {
+  it('displays the proper html', () => {
+    const component = mount(<Header />);
+    const header = component.find('h1');
+
+    expect(header.text()).toBe('Counters R Fun');
+  });
+});
+
+describe('footer component', () => {
+  it('displays the proper html', () => {
+    const component = mount(<Footer />);
+    const footer = component.find('Footer');
+
+    expect(footer.text()).toBe('\u00A9 2018 Code Fellows');
   });
 });
